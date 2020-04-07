170584cf992370d52996ca266ebd14c8a860fa76
Cristiano Ávila Salomão <xyber9@gmail.com>
2020-04-06 22:17:36 -0300

    Challenge 01
    Create an application to store repositories of the portfolio, already developed the backend in the last challenge using Node.js.

---
+++ README.md
@@ -0,0 +1,33 @@
+![header](https://raw.githubusercontent.com/salomaocristiano/gostack11-desafio-conceitos-reactjs/master/assets/header-desafios.png)
+
+<h3 align="center">
+  Desafio Rocket Seat: Conceitos do ReactJS
+</h3>
+
+## Screenshot
+
+![image-example](https://raw.githubusercontent.com/salomaocristiano/gostack11-desafio-conceitos-reactjs/master/assets/bootcamp.jpg)
+
+## :rocket: Sobre o desafio
+
+Criar uma aplicação para armazenar repositórios do portfólio, já desenvolvido o backend no último desafio utilizando o Node.js.
+
+### Funcionalidades da aplicação
+
+- **`Listar os repositórios da sua API`**: Deve ser capaz de criar uma lista com o campo **title** de todos os repositórios que estão cadastrados na sua API.
+
+- **`Adicionar um repositório a sua API`**: Deve ser capaz de adicionar um novo item na sua API através de um botão com o texto **Adicionar** e, após a criação, deve ser capaz de exibir o nome dele após o cadastro.
+
+- **`Remover um repositório da sua API`**: Para cada item da sua lista, deve possuir um botão com o texto **Remover** que, ao clicar, irá chamar uma função para remover esse item da lista do seu frontend e da sua API.
+
+### Específicação dos testes
+
+Para esse desafio temos os seguintes testes:
+
+- **`should be able to add new repository`**: Para que esse teste passe, sua aplicação deve permitir que um repositório seja adicionado ao seu backend e listado no seu frontend dentro de uma `LI`.
+
+- **`should be able to remove repository`**: Para que esse teste passe, sua aplicação deve permitir que ao clicar no botão de remover que vai estar dentro da `LI` do repositório adicionado
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(res => {
+      setRepositories(res.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const date = Date.now();
+    const res = await api.post('repositories', {
+      "title": `Título ${date}`,
+      "url": `https://github.com/${date}`,
+      "techs": [`React Native ${date}`, `Node.js ${date}`]
+    });
+
+    const repositorie = res.data;
+
+    setRepositories([...repositories, repositorie]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+    if(repositorieIndex < 0) {
+      alert('Repositório não encontrado!');
+      return;
+    }
+
+    const { status } = await api.delete(`repositories/${id}`);
+
+    if(status !== 204) {
+      alert('Não foi possível excluir o repositório!');
+      return;
+    }
+    
+    repositories.splice(repositorieIndex, 1);
+
+    setRepositories([...repositories]);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repositorie =>  (
+        <li key={repositorie.id}>
+          {repositorie.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repositorie.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
